685aaaa4584c66791ec6ac9bcd65d42b8081e820
lokesh shriwas <lokeshshriwas2802@gmail.com>
2025-07-09 23:36:54 +0530

    auth endpoint working fine

--- apps/http-backend/src/controllers/auth.controller.ts
+++ apps/http-backend/src/controllers/auth.controller.ts
@@ -1,6 +1,85 @@
-export const signupController = (req : any, res : any) => {
-    res.send("signup")
-}
-export const signinController = (req : any, res : any) => {
-    res.send("signin")
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+import { prismaClient } from "@repo/db"
+import {CreateUserSchema, SigninSchema} from "@repo/common"
+import {JWT_SECRET} from "@repo/backend-common/config"
+
+export const signupController = async (req: any, res: any) => {
+    const result = CreateUserSchema.safeParse(req.body);
+    
+    if (!result.success) {
+        return res.status(400).send({ 
+            error: "Invalid input", 
+            details: result.error.errors 
+        });
+    }
+    
+    try {
+        const { email, name, password } = result.data;
+
+        const existingUser = await prismaClient.user.findFirst({
+            where: {
+                email
+            }
+        });
+        
+        if (existingUser) {
+            return res.status(400).send("User already exists");
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        
+        const user = await prismaClient.user.create({
+            data: {
+                email,
+                name,
+                password: hashedPassword
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true,
+            }
+        });
+        
+        return res.status(201).send(user);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).send({message :"Internal server error"});
+    }
+};
+
+export const signinController = async (req : any, res : any) => {
+    const result = SigninSchema.safeParse(req.body);
+    
+    if (!result.success) {
+        return res.status(400).send({ 
+            error: "Invalid input", 
+            details: result.error.errors 
+        });
+    }
+
+    const { email, password } = result.data;
+
+    const userData = await prismaClient.user.findFirst({
+        where: {
+            email
+        }
+    })
+
+    if (!userData) {
+        return res.status(400).send({message: "User not found"});
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, userData.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).send({message: "Invalid password"});
+    }
+    if (JWT_SECRET === undefined) {
+        throw new Error('JWT_SECRET is not defined');
+    }
+    const token = jwt.sign({ userId: userData.id }, JWT_SECRET);
+    return res.status(200).send({ token : token, message: "Login successful" });
 }
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -8,8 +8,7 @@ app.use(express.json());
 
 app.use("/v1/auth", authRouter);
 
-
-app.listen(8080, async () => {
+app.listen(3002, async () => {
     // await prismaClient.$connect();
-    console.log("Server is running on http://localhost:8080");
+    console.log("Server is running on http://localhost:3002");
 });
